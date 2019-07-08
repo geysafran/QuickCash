bebce3084afa9237fe5b675e0d66f70fd915af4d
Kelvin Almeida <kelvin.f.almeida@gmail.com>
2019-07-08 15:00:52 -0300

    click pronto

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
 (function (){
+	var menuItem = document.querySelectorAll('.menu-item');
+	var menuOpen = document.querySelector('.menu-list');
+
+	var menuMobile = document.querySelector('.menu-mobile');
+	var fecharMenu = document.querySelector('.fechar-menu');
+
 	var formOpen = document.getElementById('div-form');
 	var formClose = document.getElementById('div-form');
 	
-	console.log("oioioio")
+
+
+	document.getElementById('menu-mobile').addEventListener("click", openMenu);
+	document.getElementById('fechar-menu').addEventListener("click", openMenu);
+
+  document.getElementById("menu-item").addEventListener("click", closeMenu);
+	document.getElementById("menu-item1").addEventListener("click", closeMenu);
+	document.getElementById("menu-item2").addEventListener("click", closeMenu);
 
 	document.getElementById('btnForm').addEventListener("click", openForm);
 	document.getElementById('close-form').addEventListener("click", closeForm);
 
 
+// 	Menu
+
+	function openMenu() {
+		menuOpen.classList.toggle('--menuOpen');
+		menuMobile.classList.toggle('--menuMobile');
+		fecharMenu.classList.toggle('--menuClose');
+		menuItem.forEach((element) => {
+      element.classList.add('--openItem')
+    });
+	}
+
+	function closeMenu() {
+		menuOpen.classList.remove('--isOpen');
+	}
+
+	//  Form
+
 	function openForm() {
 		formOpen.classList.toggle('--isOpen');
-		console.log('oioi')
 	}
+	
 	function closeForm() {
 		formClose.classList.remove('--isOpen');
 	}
+	
 
 })();
